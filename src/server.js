e5b38cd84ea9e1650c4d6091eaa36fc4c008f7fa
pappkartoni <flodreyer@web.de>
2023-03-30 17:15:00 +0200

    added routers to server (oops)

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import Express from "express"
 import cors from "cors"
 import {pgConnect} from "./db.js"
 import productsRouter from "./products/index.js"
+import reviewsRouter from "./reviews/index.js"
+import categoriesRouter from "./categories/index.js"
+import usersRouter from "./users/index.js"
 import { badRequestErrorHandler, genericErrorHandler, notFoundErrorHandler } from "./errors.js"
 
 const server = Express()
@@ -10,6 +13,9 @@ const port = process.env.PORT || 3420
 server.use(cors())
 server.use(Express.json())
 server.use("/products", productsRouter)
+server.use("/products", reviewsRouter)
+server.use("/categories", categoriesRouter)
+server.use("/users", usersRouter)
 server.use(badRequestErrorHandler)
 server.use(notFoundErrorHandler)
 server.use(genericErrorHandler)
